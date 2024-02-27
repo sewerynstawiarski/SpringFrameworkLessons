e313ae5fc878d6d9eac1e5d791bf9831eaeb4983
sewerynstawiarski <seweryn.stawiarski@gmail.com>
2024-02-27 12:35:40 +0100

    SpringDataRest project added

---
+++ SpringDataREST/SpringDataREST/src/main/java/com/sewerynstawiarski/SpringDataREST/domain/BeerStyleEnum.java
@@ -0,0 +1,5 @@
+package com.sewerynstawiarski.SpringDataREST.domain;
+
+public enum BeerStyleEnum {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
---
+++ SpringDataREST/SpringDataREST/src/test/java/com/sewerynstawiarski/SpringDataREST/SpringDataRestApplicationTest.java
@@ -0,0 +1,13 @@
+package com.sewerynstawiarski.SpringDataREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class SpringDataRestApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
