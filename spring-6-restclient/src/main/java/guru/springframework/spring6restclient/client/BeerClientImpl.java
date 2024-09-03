7ba84c97eec3fa8787bcd62eb36a6c4a25f90d76
sewerynstawiarski <seweryn.stawiarski@gmail.com>
2024-09-03 13:14:52 +0200

    spring 6 rest client implementation

--- spring-6-restclient/src/main/java/guru/springframework/spring6restclient/client/BeerClientImpl.java
+++ spring-6-restclient/src/main/java/guru/springframework/spring6restclient/client/BeerClientImpl.java
@@ -2,11 +2,14 @@ package guru.springframework.spring6restclient.client;
 
 
 import guru.springframework.spring6restclient.model.BeerDTO;
+import guru.springframework.spring6restclient.model.BeerDTOPageImpl;
 import guru.springframework.spring6restclient.model.BeerStyle;
 import lombok.RequiredArgsConstructor;
+import lombok.val;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.UUID;
 @Service
@@ -20,31 +23,83 @@ public class BeerClientImpl implements BeerClient {
 
     @Override
     public Page<BeerDTO> listBeers() {
-        return null;
+        return listBeers(null, null, null, null, null);
     }
 
     @Override
     public Page<BeerDTO> listBeers(String beerName, BeerStyle beerStyle, Boolean showInventory, Integer pageNumber, Integer pageSize) {
-        return null;
+        RestClient restClient = restClientBuilder.build();
+
+        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromPath(GET_BEER_PATH);
+
+        if (beerName != null) {
+            uriComponentsBuilder.queryParam("beerName",beerName);
+        }
+        if (beerStyle != null) {
+            uriComponentsBuilder.queryParam("beerStyle", beerStyle);
+        }
+        if (showInventory != null) {
+            uriComponentsBuilder.queryParam("showInventory", showInventory);
+        }
+        if (pageNumber != null) {
+            uriComponentsBuilder.queryParam("pageNumber", pageNumber);
+        }
+        if (pageSize != null) {
+            uriComponentsBuilder.queryParam("pageSize", pageSize);
+        }
+        return restClient.get()
+                .uri(uriComponentsBuilder.toUriString())
+                .retrieve()
+                .body(BeerDTOPageImpl.class);
     }
 
     @Override
     public BeerDTO getBeerById(UUID beerId) {
-        return null;
+        RestClient restClient = restClientBuilder.build();
+
+        return restClient.get()
+                .uri(uriBuilder -> uriBuilder.path(GET_BEER_BY_ID_PATH).build(beerId))
+                .retrieve()
+                .body(BeerDTO.class);
     }
 
     @Override
     public BeerDTO createBeer(BeerDTO newDto) {
-        return null;
+        RestClient restClient = restClientBuilder.build();
+
+        val location =  restClient.post()
+                .uri(uriBuilder -> uriBuilder.path(GET_BEER_PATH).build())
+                .body(newDto)
+                .retrieve()
+                .toBodilessEntity()
+                .getHeaders()
+                .getLocation();
+
+
+        return restClient.get()
+                .uri(location.getPath())
+                .retrieve()
+                .body(BeerDTO.class);
     }
 
     @Override
     public BeerDTO updateBeer(BeerDTO beerDto) {
-        return null;
+        RestClient restClient = restClientBuilder.build();
+
+         restClient.put()
+                .uri(uriBuilder -> uriBuilder.path(GET_BEER_BY_ID_PATH).build(beerDto.getId()))
+                .body(beerDto)
+                .retrieve();
+
+         return getBeerById(beerDto.getId());
     }
 
     @Override
     public void deleteBeer(UUID beerId) {
+        RestClient restClient = restClientBuilder.build();
 
+        restClient.delete()
+                .uri(uriBuilder -> uriBuilder.path(GET_BEER_BY_ID_PATH).build(beerId))
+                .retrieve();
     }
 }
--- spring-6-restclient/src/main/java/guru/springframework/spring6restclient/config/RestTemplateBuilderConfig.java
+++ spring-6-restclient/src/main/java/guru/springframework/spring6restclient/config/RestTemplateBuilderConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
 @Configuration
@@ -31,6 +32,10 @@ public class RestTemplateBuilderConfig {
         return authorizedClientManager;
     }
 
+    @Bean
+    public RestClient.Builder restClientBuilder(RestTemplateBuilder restTemplateBuilder) {
+        return RestClient.builder(restTemplateBuilder.build());
+    }
 
     @Bean
     RestTemplateBuilder restTemplateBuilder(RestTemplateBuilderConfigurer configurer,
