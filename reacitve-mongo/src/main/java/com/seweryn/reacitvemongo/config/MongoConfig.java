9c47c14746d852e1a3812ed27db8e6541a8acff8
sewerynstawiarski <seweryn.stawiarski@gmail.com>
2024-09-24 18:41:06 +0200

    adding Docker Compose

--- reacitve-mongo/src/main/java/com/seweryn/reacitvemongo/config/MongoConfig.java
+++ reacitve-mongo/src/main/java/com/seweryn/reacitvemongo/config/MongoConfig.java
@@ -1,17 +1,35 @@
 package com.seweryn.reacitvemongo.config;
 
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
 
+import java.util.Collections;
+
 @Configuration
 public class MongoConfig extends AbstractReactiveMongoConfiguration {
+    @Value("${sfg.mongohost}")
+    String mongoDbHost;
     public MongoClient mongoClient() {
         return MongoClients.create();
     }
     @Override
     protected String getDatabaseName() {
         return "sfg";
+    }
+
+    protected void configureClientSettings(MongoClientSettings.Builder builder) {
+        builder.credential(MongoCredential.createCredential("root",
+                "admin", "example".toCharArray()))
+                .applyToClusterSettings(settings -> {
+                    settings.hosts((Collections.singletonList(
+                            new ServerAddress(mongoDbHost, 27017)
+                    )));
+                });
     }
 }
