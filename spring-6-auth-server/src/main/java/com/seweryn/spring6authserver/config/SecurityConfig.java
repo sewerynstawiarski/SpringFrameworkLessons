5d833c0496214ab80d80d25cf8fc74788c7268cb
sewerynstawiarski <seweryn.stawiarski@gmail.com>
2024-09-26 19:20:17 +0200

    Kubernetes deployment and services

--- spring-6-auth-server/src/main/java/com/seweryn/spring6authserver/config/SecurityConfig.java
+++ spring-6-auth-server/src/main/java/com/seweryn/spring6authserver/config/SecurityConfig.java
@@ -150,6 +150,8 @@ public class SecurityConfig {
     }
     @Bean
     public AuthorizationServerSettings authorizationServerSettings() {
-        return AuthorizationServerSettings.builder().build();
+        return AuthorizationServerSettings.builder()
+                .issuer("http://auth-server:9000")
+                .build();
     }
 }
